3d45bd20d8d4f7562e1750c1077e2e199148da87
Eyjafjallaa <63107258+Eyjafjallaa@users.noreply.github.com>
2021-10-07 15:42:07 +0900

    created

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+const db = require('../middleware/db');
+var fileload = require('../middleware/fileload');
 /* GET home page. */
 router.get('/', async (req, res)=> {
-  res.send("something responded")
+  res.send({title:"something responded"})
+});
+
+router.post('/',fileload.single('attachmnet'),async(req, res)=> {
+    console.log(req.file);
 });
 
 module.exports = router;
